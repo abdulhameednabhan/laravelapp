f2f8b522942984e9d664fab3ddb65e3d64a37b25
abualward <abdulhameednabhan955@gmail.com>
2022-09-25 14:43:43 +0300

    the app is active

--- resources/views/student/show.blade.php
+++ resources/views/student/show.blade.php
@@ -5,7 +5,7 @@
   <div class="card-header">Students Page</div>
   <div class="card-body">
   
-        <div class="card-body">
+  <div class="card-body">
         <h5 class="card-title">Name : {{ $students->name }}</h5>
         <p class="card-text">Address : {{ $students->address }}</p>
         <p class="card-text">Mobile : {{ $students->mobile }}</p>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 use App\Http\Controllers\StudentController;
 use Illuminate\Support\Facades\Route;
+use App\Models\Student;
 
 /*
 |--------------------------------------------------------------------------
